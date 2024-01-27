5a4b129850cc02bcaac1598dc2d194845cfd9b48
HarshitJain-1908 <harshit20063@iiitd.ac.in>
2024-01-27 19:39:00 +0530

    added argobots directory & initiated argobots runtime

---
+++ hclib-light/src/argobots-runtime.c
@@ -0,0 +1,11 @@
+//Our own implementation of Argobots runtime with custom work stealing
+//scheduler for User level thread (ULTs).
+
+#include <stdio.h>
+#include <abt.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello HCArgoLib \n");
+    return 0;
+}
