eb1bed68606962fbfb55158a47b06496a7a93cc8
HarshitJain-1908 <harshit20063@iiitd.ac.in>
2024-01-27 14:49:09 +0530

    Initial commit

---
+++ argobots-runtime.c
@@ -0,0 +1,10 @@
+//Our own implementation of Argobots runtime with custom work stealing scheduler
+
+#include <stdio.h>
+// #include <abt.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello HCArgoLib \n");
+    return 0;
+}
