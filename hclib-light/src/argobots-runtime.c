bde51fe16da75cd60c4df1daceeacc5ad45f69ca
HarshitJain-1908 <harshit20063@iiitd.ac.in>
2024-01-27 20:04:28 +0530

    runtime initialization

--- hclib-light/src/argobots-runtime.c
+++ hclib-light/src/argobots-runtime.c
@@ -2,10 +2,32 @@
 //scheduler for User level thread (ULTs).
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 #include <abt.h>
+
+#define DEFAULT_NUM_XSTREAMS 2
+#define DEFAULT_NUM_THREADS 8
+
+typedef struct {
+    int tid;
+} thread_arg_t;
+
+void hello_world(void *arg)
+{
+    int tid = ((thread_arg_t *)arg)->tid;
+    int rank;
+    //ABT_xstream_self_rank(&rank);
+    printf("Hello world! (thread = %d, ES = %d)\n", tid, rank);
+}
+
 
 int main(int argc, char **argv) {
 
-    printf("Hello HCArgoLib\n");
+    printf("Hello! HCArgoLib\n");
+    printf("Number of ES = %d \n", DEFAULT_NUM_XSTREAMS);
+    printf("Number of ULTs = %d \n", DEFAULT_NUM_THREADS);
+
     return 0;
 }
