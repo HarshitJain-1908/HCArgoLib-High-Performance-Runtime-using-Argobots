7bff7890ecf2b6dc5809a2137487783dd9b13a20
VanshikaJain1601 <vanshikajain160601@gmail.com>
2024-02-01 15:24:10 +0530

    test-flat-finish

---
+++ hclib-light/test/prime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "hclib.hpp"
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    hclib::init();
+    hclib::finish([](){
+        for(int i=0; i<100; i++){
+            hclib::async([](){cout<<"Lambda function called\n";});
+            // std::this_thread::sleep_for(std::chrono::milliseconds(3));
+         }
+    } );
+    hclib::finalize();
+
+    return 0;
+}
